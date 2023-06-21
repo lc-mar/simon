a1cf7317483d23340681485041e72e67990f4a3d
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-21 10:12:58 -0400

    moved lines over to proper function

--- game.js
+++ game.js
@@ -16,10 +16,10 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-}
-function playSound(name){
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
+}
+function playSound(name){
     var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
 }
