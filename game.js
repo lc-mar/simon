26b1bd509de02220d8656385a0ce0df19b50b49a
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-21 10:11:39 -0400

    adding other function and modifying

--- game.js
+++ game.js
@@ -7,7 +7,8 @@ var userClickedPattern = [];
 
 $(".btn").click(function() {
   var userChosenColor = $(this).attr("id");
-  userClickedPattern.push(userChosenColor);
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
 });
 
 function nextSequence() {
@@ -15,8 +16,10 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-
+}
+function playSound(name){
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
     var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
 }
