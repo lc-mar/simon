5bdeb4d1529978796dee78b2f3e575fd21bfb0da
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-21 10:15:56 -0400

    reformatted prior js and added new function

--- game.js
+++ game.js
@@ -5,10 +5,12 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
-$(".btn").click(function() {
+$(".btn").click(function () {
+    
   var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
+    animatePress(userChosenColor);
 });
 
 function nextSequence() {
@@ -16,10 +18,16 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
     playSound(randomChosenColor);
 }
 function playSound(name){
     var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColor) {
+    
 }
