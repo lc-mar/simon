34376459a0f9ffa7635f1c7fb99ed715a1e6c709
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-21 09:46:33 -0400

    added random number var

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+
+//1. Inside game.js create a new function called nextSequence()
 function nextSequence() {
-    
-}
+    //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
+    var randomNumber = Math.floor(Math.random() * 4);
+};
