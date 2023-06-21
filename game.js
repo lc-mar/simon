6d54588da8ae361a27a514664309c3fed297089d
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-21 09:48:27 -0400

    game pattern var added

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 //3. At the top of the game.js file, create a new array called buttonColors and set it to hold the sequence "red", "blue", "green", "yellow" .
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+//5. At the top of the game.js file, create a new empty array called gamePattern.
+var gamePattern = [];
+
 //1. Inside game.js create a new function called nextSequence()
 function nextSequence() {
     //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
