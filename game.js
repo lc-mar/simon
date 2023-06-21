901043cda532bc241eb97af5296ce9051604923d
Lauren <78833615+lc-mar@users.noreply.github.com>
2023-06-21 10:09:37 -0400

    Update game.js
    added closing tags

--- game.js
+++ game.js
@@ -3,6 +3,13 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+});
+
 function nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
